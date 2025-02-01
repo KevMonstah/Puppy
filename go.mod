1d8aa71797cd18bd4740a77dadff62301fcbdf1c
kevmonstah <kevmonstah@gmail.com>
2025-02-01 16:55:21 -0500

    adding Dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KevMonstah/Puppy
 
 go 1.23.5
+
+require github.com/KevMonstah/Dog v0.0.0-20250201215044-309b159885c3
--- puppy.go
+++ puppy.go
@@ -2,6 +2,8 @@ package Puppy
 
 import (
 	"log"
+
+	"github.com/KevMonstah/Dog"
 )
 
 func Bark() string {
@@ -12,4 +14,22 @@ func Bark() string {
 func Barks() string {
 	log.Println("Puppy Bark Bark Bark")
 	return "Puppy.Barks a lot!!!"
+}
+
+func Sleeps(s string) string {
+	log.Println("Puppy sleeps")
+	Dog.Sleep("Cinnamon")
+	return "My Puppy sleeps"
+}
+
+func Runs(s string) string {
+	log.Println(s + " the dog runs")
+	Dog.Runs("Runes")
+	return "My Puppy Runs"
+}
+
+func Chills(s string) string {
+	log.Println(s + "the dog is chilling")
+	Dog.Sleep("Nunes")
+	return "My Puppy Chills"
 }
