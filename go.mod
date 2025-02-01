2252b72def2a271c4543563ff6c693b6660ebefa
kevmonstah <kevmonstah@gmail.com>
2025-02-01 16:33:07 -0500

    deps suck

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KevMonstah/Puppy
+
+go 1.23.5
---
+++ puppy.go
@@ -0,0 +1,15 @@
+package Puppy
+
+import (
+	"log"
+)
+
+func Bark() string {
+	log.Println("Puppy Bark")
+	return "Puppy.Bark"
+}
+
+func Barks() string {
+	log.Println("Puppy Bark Bark Bark")
+	return "Puppy.Barks a lot!!!"
+}
